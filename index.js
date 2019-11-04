fed42725a6f922c1da94d94ef0b00548c0f97f1c
Avijeet Singh <avitomar99@gmail.com>
2019-11-04 09:37:42 -0500

    commit

--- index.js
+++ index.js
@@ -1,6 +1,42 @@
 var http = require('http');
 var Datastore = require('nedb');
-var db = new Datastore( { inMemoryOnly: true });
+//var db = new Datastore( { inMemoryOnly: true });
+
+/*var db = new Datastore({ filename: 'users.json' })
+db.loadDatabase(function(err) {
+    // Start issuing commands after callback...
+});*/
+
+var users = new Datastore();
+
+var people = [];
+
+var scott = {
+    name: 'Scott Robinson',
+    age: 28,
+    twitter: '@ScottWRobinson'
+};
+
+var elon = {
+    name: 'Elon Musk',
+    age: 44,
+    twitter: '@elonmusk'
+};
+
+var jack = {
+    name: 'Jack Dorsey',
+    age: 39,
+    twitter: '@jack'
+};
+
+people.push(scott, elon, jack);
+
+users.insert(people, function(err, docs) {
+    docs.forEach(function(d) {
+        console.log('Saved user:', d.name);
+    });
+});
+
 var ODataServer = require('simple-odata-server');
 var Adapter = require('simple-odata-server-nedb');
  
